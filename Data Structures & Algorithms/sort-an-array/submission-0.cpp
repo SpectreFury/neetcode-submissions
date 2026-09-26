431beebb8a293765b4124fb9758af137e5ffdf1c
Ayush Soni <97754210+SpectreFury@users.noreply.github.com>
2026-09-26 22:58:44 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = 0; j < nums.size(); j++) {
+                if(nums[i] < nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+
+        return nums;
+    }
+};
